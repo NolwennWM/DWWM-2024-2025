b64f20fcb321d5ae6bb7913f0640d6faa9abb69d
Nolwenn <nolwenn.wm@afci-formation.fr>
2025-02-03 12:00:11 +0100

    symfony doctrine et CRUD

--- 07-php/08-symfony/projet_cours/src/Controller/HomeController.php
+++ 07-php/08-symfony/projet_cours/src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 // #[Route("/user")]
 final class HomeController extends AbstractController
@@ -21,17 +22,36 @@ final class HomeController extends AbstractController
             "xss"=>"<script>alert('coucou');</script>"
         ]);
     }
-    #[Route("/bonjour/anglais/{username}", name:'app_hello')]
+    #[Route("/bonjour/anglais/{username<^[a-zA-Z]+$>?John}", name:'app_hello')]
     public function hello($username): Response
     {
+        $this->addFlash("bonjour", "Hello $username");
+        $this->addFlash("redirection", "Vous avez été redirigé !");
+
 
         return $this->redirectToRoute("app_bonjour", ["nom"=>"Smith", "prenom"=>$username]);
     }
-    #[Route("/bonjour/{nom}/{prenom}", name:'app_bonjour')]
-    public function bonjour($nom, $prenom): Response
+    /* 
+        /bonjour/dupont --> /bonjour/dupont/jean
+    */
+    #[Route("/bonjour/{nom}/{prenom}", name:'app_bonjour', defaults:["prenom"=>"Jean"], requirements:["prenom"=>"^[a-zA-Z]+$", "nom"=>"^[a-zA-Z]+$"])]
+    public function bonjour($nom, $prenom, Request $request): Response
     {
-        dump($nom, $prenom);
+        dump($request);
         // dd($nom, $prenom);
+        // Compteur de visite sur la page
+        $sess = $request->getSession();
+        if($sess->has("nbVisite"))
+        {
+            $nbVisite = $sess->get("nbVisite")+1;
+        }else
+        {
+            $nbVisite = 1;
+        }
+        $sess->set("nbVisite", $nbVisite);
+        // message flash
+        $this->addFlash("bonjour", "Bonjour $prenom $nom");
+
         return $this->render("home/bonjour.html.twig", [
             "nom"=>$nom,
             "prenom"=>$prenom
---
+++ 07-php/08-symfony/projet_cours/src/Controller/MessageController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Message;
+use App\Repository\MessageRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route("/message")]
+final class MessageController extends AbstractController
+{
+    #[Route('/add', name: 'app_message_create')]
+    public function createMessage(ManagerRegistry $doc): Response
+    {
+        $em = $doc->getManager();
+        $message = new Message();
+        $message->setContent("Ceci est un message de test")
+                ->setCreatedAt(new \DateTimeImmutable());
+        $em->persist($message);
+        $em->flush();
+
+        return $this->render('message/index.html.twig', [
+            'controller_name' => 'MessageController',
+        ]);
+    }
+
+    #[Route("/{page<^\d+$>?1}/{nb<^\d+$>?5}", name: "app_message_read")]
+    // public function readMessage(ManagerRegistry $doc): Response
+    public function readMessage(MessageRepository $repo, int $page, int $nb): Response
+    {
+        // $repo = $doc->getRepository(Message::class);
+        // $messages = $repo->findAll();
+        // $messages = $repo->find(/*un id*/);
+        // $messages = $repo->findOneBy([]);
+        // $messages = $repo->findBy([], ['createdAt' => 'DESC']);
+        // $messages = $repo->findByDateInterval("2020-01-01","2025-02-03");
+        $messages = $repo->findBy([], ['createdAt' => 'DESC'], $nb, ($page-1)*$nb);
+        $total = $repo->count();
+        $nbPage = ceil($total/$nb);
+        return $this->render('message/index.html.twig', [
+            'messages' => $messages,
+            'nbPage' => $nbPage,
+            'currentPage' => $page,
+            'nombre'=>$nb
+        ]);
+    }
+    #[Route("/delete/{id<^\d+$>}", name:"app_message_delete")]
+    // public function deleteMessage(ManagerRegistry $doc, int $id): Response
+    public function deleteMessage(ManagerRegistry $doc, MessageRepository $repo, int $id): Response
+    {
+        // $repo = $doc->getRepository(Message::class);
+        $message = $repo->find($id);
+        if(!$message)
+        {
+            $this->addFlash("error", "Aucun message correspondant");
+        }else
+        {
+            $em = $doc->getManager();
+            $em->remove($message);
+            $em->flush();
+            $this->addFlash("info", "Message supprimé avec succès");
+        }
+        return $this->redirectToRoute("app_message_read");
+    }
+    #[Route("/update/{id<^\d+$>}", name: "app_message_update")]
+    public function updateMessage(Message $message=null, ManagerRegistry $doc):Response
+    {
+        // dd($message);
+        if(!$message)
+        {
+            $this->addFlash("error", "Aucun message correspondant");
+        }else
+        {
+            $message->setContent("Message Modifié")
+                    ->setEditedAt(new \DateTime());
+            $em = $doc->getManager();
+            $em->persist($message);
+            $em->flush();
+            $this->addFlash("info", "Message modifié avec succès");
+        }
+        return $this->redirectToRoute("app_message_read");
+    }
+}
