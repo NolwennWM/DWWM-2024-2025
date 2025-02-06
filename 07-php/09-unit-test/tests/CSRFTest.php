e0d26ec011ef5931df4c31dd68c96adbe2a64c00
Nolwenn <nolwenn.wm@afci-formation.fr>
2025-02-06 10:37:00 +0100

    test unitaires

---
+++ 07-php/09-unit-test/tests/CSRFTest.php
@@ -0,0 +1,23 @@
+<?php 
+declare(strict_types=1);
+require __DIR__. "/../../ressources/service/_csrf.php";
+use PHPUnit\Framework\TestCase;
+
+final class CSRFTest extends TestCase
+{
+    public function testCanSetCSRF()
+    {
+        setCSRF();
+        $this->assertArrayHasKey("token", $_SESSION, "Le jeton CSRF a bien été créé");
+    }
+    public function testCanConfirmCSRF()
+    {
+        $_POST["token"] = $_SESSION["token"];
+        $this->assertTrue(isCSRFValid(), "Le jeton CSRF est valide");
+    }
+    public function testCannotConfirmCSRF()
+    {
+        $_POST["token"] = "chausette";
+        $this->assertFalse(isCSRFValid(), "Le jeton CSRF est invalide");
+    }
+}
